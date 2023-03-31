43db33bd7098f7f628edd3958904492ea717e07c
Mateus Farias <mateused51@hotmail.com>
2023-03-31 11:44:05 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const sobre = document.querySelector('.sobre')
+const habilidade = document.querySelector('.habilidades')
+
+const buttonSobre = document.querySelector('.buttonSobre')
+const buttonHabilidade = document.querySelector('.buttonHabilidade')
+
+
+function buttonS(){
+    sobre.style.display = 'block'
+    habilidade.style.display = 'none'
+
+    buttonSobre.classList.add('buttonSelected')
+    buttonHabilidade.classList.remove('buttonSelected')
+}
+
+function buttonH(){
+    sobre.style.display = 'none'
+    habilidade.style.display = 'flex'
+
+    buttonSobre.classList.remove('buttonSelected')
+    buttonHabilidade.classList.add('buttonSelected')
+}
+
+function buttonC(){
+    sobre.style.display = 'none'
+    habilidade.style.display = 'none'
+
+    buttonSobre.classList.remove('buttonSelected')
+    buttonHabilidade.classList.remove('buttonSelected')
+}
